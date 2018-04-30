a63f3b5e2aed0393adedf63eb5405ef7298f8d00
Colin Gillette <colingillette35@gmail.com>
2018-04-30 10:05:19 -0500

    init commit

---
+++ StudentForm/StudentForm/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<Student> students = new List<Student>();
+
+            Student s1 = new Student();
+            Student s2 = new Student("Doe", "John", "M01020304");
+            Student s3 = new Student("Dane", "Jane", "M12345678", "Female", "08/23/1996", "Communications");
+            Student s4 = new Student("Horton", "William", "M2254879", "Male", "12/23/1997", "Actuarial Studies");
+            Student s5 = new Student("Lange", "Alexa", "M55887744", "Female", "03/06/1995", "Finance");
+
+            students.Add(s1);
+            students.Add(s2);
+            students.Add(s3);
+            students.Add(s4);
+            students.Add(s5);
+
+            for (int i = 0; i < students.Count; i++)
+            {
+                MessageBox.Show(students[i].printStudentInfo());
+            }
+        }
+    }
+}
---
+++ StudentForm/StudentForm/Student.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentForm
+{
+    class Student
+    {
+        private string _lastName;
+        private string _firstName;
+        private string _studentID;
+        private string _gender;
+        private string _dateOfBirth;
+        private string _major;
+
+        public Student()
+        {
+            _lastName = "unknown";
+            _firstName = "unknown";
+            _studentID = "M00000000";
+            _gender = "unknown";
+            _dateOfBirth = "01/01/1900";
+            _major = "undecided";
+        }
+
+        public Student(string lastName, string firstName, string studentID)
+        {
+            _lastName = lastName;
+            _firstName = firstName;
+            _studentID = studentID;
+            _gender = "unknown";
+            _dateOfBirth = "01/01/1900";
+            _major = "undecided";
+        }
+
+        public Student(string lastName, string firstName, string studentID, string gender, string dateOfBirth, string major)
+        {
+            _lastName = lastName;
+            _firstName = firstName;
+            _studentID = studentID;
+            _gender = gender;
+            _dateOfBirth = dateOfBirth;
+            _major = major;
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        public string StudentID
+        {
+            get { return _studentID; }
+            set { _studentID = value; }
+        }
+
+        public string Gender
+        {
+            get { return _gender; }
+            set { _gender = value; }
+        }
+
+        public string DateOfBirth
+        {
+            get { return _dateOfBirth; }
+            set { _dateOfBirth = value; }
+        }
+
+        public string Major
+        {
+            get { return _major; }
+            set { _major = value; }
+        }
+
+        public string printStudentInfo()
+        {
+            string desc = $"{FirstName} {LastName} ({Gender}) \n{StudentID} {Major} \nBorn: {DateOfBirth}";
+            return desc;
+        }
+    }
+}
